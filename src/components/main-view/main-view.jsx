cb27c50198b2ce1ff88ca95a61e4e1d8acfa316a
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-07-18 08:20:35 -0700

    Login and Signup view
    - created login and signup views with input forms
    -added validation and authentication measures

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+
+export const MainView = () => {  
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");  
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+    const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        fetch("https://my-flix-caleb-7e8e5b64a2c6.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        .then((response) => response.json())
+        .then((movies) => {
+            setMovies(movies);
+        });  
+
+    }, [token]);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView 
+                    onLoggedIn={(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                }}/>
+                or
+                <SignupView />
+            </>
+        );
+    }
+    
+    if (selectedMovie) {
+        return ( 
+            <>
+                <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+                <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+            </>
+        );
+    }
+
+    if (movies.length === 0) {
+        return (
+            <>
+                <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+                <div>The list is empty!</div>;
+            </>
+        );
+    }
+
+    return (
+        <div>
+            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+            {movies.map((movie) => (
+                <MovieCard 
+                    key={movie.id}
+                    movie={movie} 
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
+            ))}
+        </div>
+    );
+};
---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (
+        <div 
+            onClick={() => {
+                onMovieClick(movie);
+            }}
+        >
+            {movie.title}
+        </div>
+    );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        image: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
