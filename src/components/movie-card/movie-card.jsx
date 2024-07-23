b3740bdaf49705bec2e685fb39fc5db426e70c6a
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-07-23 09:56:30 -0700

    Squashed commit of the following:
    commit 198b45c17586e9df0af11fd88425585e08b7b72a
    Author: CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
    Date:   Fri Jul 19 14:32:50 2024 -0700
    
        UI Styling
    
        - added react-bootstrap forms and grid system

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
+import "./movie-card.scss"
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <div 
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
-            {movie.title}
-        </div>
+        <Card className="h-100" onClick={() => onMovieClick}>
+            <Card.Img variant="top" src={useBootstrapBreakpoints.image} />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{movie.director}</Card.Text>
+                <Button onClick={() => onMovieClick(book)} variant="link">Open</Button>
+            </Card.Body>
+        </Card>
     );
 };
 
